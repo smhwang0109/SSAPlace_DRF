ec7176cbc016f2f4fe534bdba15b2535250d5605
SunHwan-Park <57381062+SunHwan-Park@users.noreply.github.com>
2020-06-27 03:09:30 +0900

    Boards - Build REST API

--- boards/urls.py
+++ boards/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = 'boards'
 
 urlpatterns = [
-    
+    path('<int:board_id>/', views.ArticleListView.as_view()),
+    path('articles/<int:article_id>/', views.ArticleDetailView.as_view()),
+    path('articles/<int:article_id>/comments/', views.ArticleCommentListView.as_view()),
+    path('articles/<int:article_id>/comments/<int:comment_id>/', views.ArticleCommentDetailView.as_view()),
+    path('articles/<int:article_id>/like/', views.ArticleLikeView.as_view()),
+    path('articles/search/<str:keyword>/', views.ArticleSearchView.as_view()),
 ]
