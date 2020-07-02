598ae2ac0ecf36ec3e77656ab4413fafdf470045
scl2589 <chaelinshin96@gmail.com>
2020-07-02 16:46:43 +0900

    Edited time format - created_at, updated_at

--- boards/serializers.py
+++ boards/serializers.py
@@ -43,6 +43,8 @@ class FreeArticleListSerializer(serializers.ModelSerializer):
 
 class FreeArticleDetailSerializer(serializers.ModelSerializer):    
     author = UserSerializer(required=False)
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M", required=False)
+    updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M", required=False)
     free_comments = serializers.SerializerMethodField()
     class Meta:
         model = FreeArticle
