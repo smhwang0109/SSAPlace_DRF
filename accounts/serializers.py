7083471f4a1033ad7e34157c6815835025249260
scl2589 <chaelinshin96@gmail.com>
2020-07-04 03:29:49 +0900

    Update Profile Edit View - still working on it

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -20,3 +20,36 @@ class UserSerializer(serializers.ModelSerializer):
     #     return django_serializers.serialize('json', obj.like_articles.order_by('-created_at'), ensure_ascii=False)
     
     
+class UserProfileSerializer(serializers.ModelSerializer):
+    # 자기소개, 위치, 웹사이트 주소 (페이스북, 인스타그램, github, 개인홈페이지, linkedin), email, languages, skills, team
+    # self_introduction = serializers.CharField()
+    # location = serializers.CharField()
+    # email = serializers.EmailField()
+    # instagram = serializers.URLField()
+    # github = serializers.URLField() 
+    # facebook = serializers.URLField()
+    # homepage = serializers.URLField()
+    # linkedin = serializers.URLField() 
+    # teams = serializers.SerializerMethodField()
+    # languages = serializers.SerializerMethodField()
+    # skills = serializers.SerializerMethodField()
+    class Meta:
+        model = User
+        exclude = ['password',]
+    
+    def get_teams(self, obj):
+        team = obj.teams.all() 
+        return django_serializers.serializer('json', team)
+    
+    # def get_skills(self, obj):
+    #     skill = obj.
+    # articles = serializers.SerializerMethodField()
+    # like_articles = serializers.SerializerMethodField()
+
+    # def get_articles(self, obj):
+    #     return django_serializers.serialize('json', obj.articles.order_by('-created_at'), ensure_ascii=False)
+
+    # def get_like_articles(self, obj):
+    #     return django_serializers.serialize('json', obj.like_articles.order_by('-created_at'), ensure_ascii=False)
+    
+    
--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import get_user_model
 
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, UserProfileSerializer
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -18,3 +18,19 @@ class UserListView(APIView):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
+
+class Profile(APIView):
+    def get(self, request):
+        user = request.user
+        serializer = UserProfileSerializer(user)
+        return Response(serializer.data)
+
+class ProfileEdit(APIView):
+    def post(self, request):
+        user = request.user
+        serializer = UserProfileSerializer(user)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user)
+            User = get_user_model()
+            return Response(serializer.data)
+        return Response(serializer.errors)
