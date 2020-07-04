de7e4149a4c8ef09bce0f97c7872b01f3ed45ca8
Soomin Hwang <smhwang0109@gmail.com>
2020-07-05 03:28:08 +0900

    Teams - Add CollectTeamCreate & Modified teams models

--- teams/urls.py
+++ teams/urls.py
@@ -7,14 +7,11 @@ urlpatterns = [
     path('', views.TeamListView.as_view()),
     path('<int:team_id>/', views.TeamDetailView.as_view()),
     path('<int:team_id>/collect/', views.CollectTeamCreateView.as_view()),
-    path('<int:team_id>/collect/<int:collect_team_id>/', views.CollectMemberCreateView.as_view()),
     path('collect/', views.CollectTeamListView.as_view()),
     path('collect/<int:collect_team_id>/', views.CollectTeamDetailView.as_view()),
 
     # 부분 모델 가져오기
-    # Interest, Role, Major, UseLanguage
+    # Interest, UseLanguage
     path('interest/', views.InterestListView.as_view()),
-    path('role/', views.RoleListView.as_view()),
-    path('major/', views.MajorListView.as_view()),
     path('language/', views.UseLanguageListView.as_view()),
 ]
